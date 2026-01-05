bdf27d97578c82912aa376b42754f1c40ffd7d72
vlldnt <vieilledent.adrien@gmail.com>
2026-01-05 10:21:46 +0000

    fix: version 2.8 redis ffs

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -6,26 +6,46 @@ export class RedisClient {
     this.client.on("error", (err) => {
       console.error(`Redis client error: ${err}`);
     });
-    this.client.connect().catch((err) => {
-      console.error(`Redis connection error: ${err}`);
-    });
   }
 
   isAlive() {
-    return this.client.isOpen;
+    return this.client.connected;
   }
 
   async get(key) {
-    const value = await this.client.get(key);
-    return value;
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
+    });
   }
 
   async set(key, value, duration) {
-    await this.client.set(key, value, { EX: duration });
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
   }
 
   async del(key) {
-    await this.client.del(key);
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
   }
 }
 
